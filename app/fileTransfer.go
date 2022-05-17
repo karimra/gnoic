4aefb2e09cc04f076cfef2ddd0ab4a7354f2afeb
Karim Radhouani <medkarimrdi@gmail.com>
2022-05-16 21:43:31 -0700

    implement file transfer RPC

--- app/fileTransfer.go
+++ app/fileTransfer.go
@@ -1,15 +1,31 @@
 package app
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"strings"
 
+	"github.com/olekukonko/tablewriter"
+	"github.com/openconfig/gnoi/common"
+	"github.com/openconfig/gnoi/file"
+	"github.com/openconfig/gnoi/types"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"google.golang.org/grpc/metadata"
 )
+
+type fileTransferResponse struct {
+	TargetError
+	rsp *file.TransferToRemoteResponse
+}
 
 func (a *App) InitFileTransferFlags(cmd *cobra.Command) {
 	cmd.ResetFlags()
 	//
+	cmd.Flags().StringVar(&a.Config.FileTransferLocal, "local", "", "path to local target file to be transferred")
+	cmd.Flags().StringVar(&a.Config.FileTransferRemote, "remote", "", "remote path to transfer the local file to")
+	cmd.Flags().StringVar(&a.Config.FileTransferSourceAddress, "source-address", "", "source address used to initiate connections from the target")
 	//
 	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
 		a.Config.FileConfig.BindPFlag(fmt.Sprintf("%s-%s", cmd.Name(), flag.Name), flag)
@@ -17,6 +33,129 @@ func (a *App) InitFileTransferFlags(cmd *cobra.Command) {
 }
 
 func (a *App) RunEFileTransfer(cmd *cobra.Command, args []string) error {
-	fmt.Println("not implemented")
-	return nil
+	targets, err := a.GetTargets()
+	if err != nil {
+		return err
+	}
+
+	numTargets := len(targets)
+	responseChan := make(chan *fileTransferResponse, numTargets)
+	a.wg.Add(numTargets)
+	for _, t := range targets {
+		go func(t *Target) {
+			defer a.wg.Done()
+			ctx, cancel := context.WithCancel(a.ctx)
+			defer cancel()
+			ctx = metadata.AppendToOutgoingContext(ctx, "username", *t.Config.Username, "password", *t.Config.Password)
+
+			err = a.CreateGrpcClient(ctx, t, a.createBaseDialOpts()...)
+			if err != nil {
+				responseChan <- &fileTransferResponse{
+					TargetError: TargetError{
+						TargetName: t.Config.Address,
+						Err:        err,
+					},
+				}
+				return
+			}
+			responseChan <- a.FileTransfer(ctx, t)
+		}(t)
+	}
+	a.wg.Wait()
+	close(responseChan)
+
+	errs := make([]error, 0, numTargets)
+	result := make([]*fileTransferResponse, 0, numTargets)
+	for rsp := range responseChan {
+		if rsp.Err != nil {
+			wErr := fmt.Errorf("%q File Stat failed: %v", rsp.TargetName, rsp.Err)
+			a.Logger.Error(wErr)
+			errs = append(errs, wErr)
+			continue
+		}
+		result = append(result, rsp)
+	}
+	for _, r := range result {
+		a.printMsg(r.TargetName, r.rsp)
+	}
+	fmt.Print(a.transferTable(result))
+	return a.handleErrs(errs)
+}
+
+func (a *App) FileTransfer(ctx context.Context, t *Target) *fileTransferResponse {
+	rd, err := a.transferFileRemoteDownload()
+	if err != nil {
+		return &fileTransferResponse{
+			TargetError: TargetError{
+				TargetName: t.Config.Name,
+				Err:        err,
+			},
+		}
+	}
+	req := &file.TransferToRemoteRequest{
+		LocalPath:      a.Config.FileTransferLocal,
+		RemoteDownload: rd,
+	}
+	fileClient := file.NewFileClient(t.client)
+	a.Logger.Infof("sending file transfer request: %v to target %q", req, t.Config.Name)
+	rsp, err := fileClient.TransferToRemote(ctx, req)
+	return &fileTransferResponse{
+		TargetError: TargetError{
+			TargetName: t.Config.Name,
+			Err:        err,
+		},
+		rsp: rsp,
+	}
+}
+
+func (a *App) transferTable(r []*fileTransferResponse) string {
+	targetTabData := make([][]string, 0)
+	for _, rsp := range r {
+		targetTabData = append(targetTabData, []string{rsp.TargetName, rsp.rsp.Hash.Method.String(), fmt.Sprintf("%x", rsp.rsp.Hash.Hash)})
+	}
+	b := new(bytes.Buffer)
+	table := tablewriter.NewWriter(b)
+	table.SetHeader([]string{"Target Name", "Hash Method", "Hash"})
+	formatTable(table)
+	table.AppendBulk(targetTabData)
+	table.Render()
+	return b.String()
+}
+
+// scp://user:pass@server.com:/path/to/file
+// sftp://user:pass@server.com:/path/to/file
+// http(s)://user:pass@server.com/path/to/file
+func (a *App) transferFileRemoteDownload() (*common.RemoteDownload, error) {
+	path := a.Config.FileTransferRemote
+	rd := new(common.RemoteDownload)
+	switch {
+	case strings.HasPrefix(path, "https://"):
+		rd.Protocol = common.RemoteDownload_HTTPS
+		path = strings.TrimPrefix(path, "https://")
+	case strings.HasPrefix(path, "http://"):
+		rd.Protocol = common.RemoteDownload_HTTP
+		path = strings.TrimPrefix(path, "http://")
+	case strings.HasPrefix(path, "scp://"):
+		rd.Protocol = common.RemoteDownload_SCP
+		path = strings.TrimPrefix(path, "scp://")
+	case strings.HasPrefix(path, "sftp://"):
+		rd.Protocol = common.RemoteDownload_SFTP
+		path = strings.TrimPrefix(path, "sftp://")
+	default:
+		return nil, fmt.Errorf("missing protocol in remote path %q", path)
+	}
+	if strings.Contains(path, "@") {
+		rd.Credentials = &types.Credentials{}
+		pathElems := strings.SplitN(path, "@", 2)
+		credentials := strings.SplitN(pathElems[0], ":", 2)
+		if len(credentials) != 2 {
+			return nil, fmt.Errorf("failed to parse remote path credentials %q", a.Config.FileTransferRemote)
+		}
+		rd.Credentials.Username = credentials[0]
+		rd.Credentials.Password = &types.Credentials_Cleartext{Cleartext: credentials[1]}
+		path = strings.TrimPrefix(path, fmt.Sprintf("%s:%s@", credentials[0], credentials[1]))
+	}
+	rd.Path = path
+	rd.SourceAddress = a.Config.FileTransferSourceAddress
+	return rd, nil
 }
--- docs/command_reference/file/transfer.md
+++ docs/command_reference/file/transfer.md
@@ -2,10 +2,54 @@
 
 ### Description
 
-Not implemented
+The `file transfer` command uses the [gNOI File Transfer RPC](https://github.com/openconfig/gnoi/blob/master/file/file.proto#L41) to transfer a file from a target to a remote location.
+
+It supports 2 flags:
+
+- `local`: the file name local to the target.
+- `remote`: the remote location to which the target should transfer the file.
 
 ### Usage
 
+`gnoic [global-flags] file transfer [local-flags]`
+
 ### Flags
 
+#### local
+
+The `--local` sets the local file path to be transferred from the target to the remote location.
+
+#### remote
+
+The `--remote` flag is used to set the remote location to which the target should transfer the local file.
+
+It should include the protocol used, any credentials (if needed), the remote address and path.
+
+e.g:
+
+- `scp://user:pass@server.com:/path/to/file`
+- `scp:/192.168.1.1:/path/to/file`
+- `sftp://user:pass@server.com:/path/to/file`
+- `http://user:pass@server.com/path/to/file`
+- `https://user:pass@server.com/path/to/file`
+- `https://server.com/path/to/file`
+
+#### source-address
+
+The `--source-address` flag sets the source address used to initiate connections from the target.
+It can be either an IPv4 address or an IPv6 address, depending on the connection's destination address.
+
 ### Examples
+
+```bash
+gnoic -a clab-gnoi-sr1 --insecure -u admin -p admin file transfer --path cf3:\bof.cfg --remote http://admin:admin@172.100.100.1:8000/bof.cfg
+```
+
+```
+INFO[0000] sending file transfer request: local_path:"cf3:bof.cfg" remote_download:{path:"172.100.100.1:8000/bof.cfg" protocol:HTTP} to target "clab-gnoi-sr1:57400" 
++---------------------+-------------+----------------------------------+
+|     Target Name     | Hash Method |               Hash               |
++---------------------+-------------+----------------------------------+
+| clab-gnoi-sr1:57400 | MD5         | ce226201074f995db2c2a88a6948fb51 |
++---------------------+-------------+----------------------------------+
+```
