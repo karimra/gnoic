90f027f93e2b5983d8c9095e5969c2f9622ac3d2
Karim Radhouani <medkarimrdi@gmail.com>
2023-03-10 00:37:03 -0800

    address reviews

--- app/services.go
+++ app/services.go
@@ -10,7 +10,6 @@ import (
 	"github.com/karimra/gnoic/api"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"google.golang.org/grpc/metadata"
 	reflectpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 )
@@ -20,14 +19,6 @@ type reflectionResponse struct {
 	rsp *reflectpb.ServerReflectionResponse
 }
 
-func (a *App) InitServicesFlags(cmd *cobra.Command) {
-	cmd.ResetFlags()
-	//
-	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		a.Config.FileConfig.BindPFlag(fmt.Sprintf("%s-%s", cmd.Name(), flag.Name), flag)
-	})
-}
-
 func (a *App) RunEServices(cmd *cobra.Command, args []string) error {
 	targets, err := a.GetTargets()
 	if err != nil {
@@ -35,59 +26,10 @@ func (a *App) RunEServices(cmd *cobra.Command, args []string) error {
 	}
 	numTargets := len(targets)
 	responseChan := make(chan *reflectionResponse, numTargets)
+
 	a.wg.Add(numTargets)
 	for _, t := range targets {
-		go func(t *api.Target) {
-			defer a.wg.Done()
-			ctx, cancel := context.WithCancel(a.ctx)
-			defer cancel()
-			ctx = metadata.AppendToOutgoingContext(ctx, "username", *t.Config.Username, "password", *t.Config.Password)
-
-			err = t.CreateGrpcClient(ctx, a.createBaseDialOpts()...)
-			if err != nil {
-				responseChan <- &reflectionResponse{
-					TargetError: TargetError{
-						TargetName: t.Config.Name,
-						Err:        err,
-					},
-				}
-				return
-			}
-			defer t.Close()
-
-			rfc := reflectpb.NewServerReflectionClient(t.Conn())
-			info, err := rfc.ServerReflectionInfo(ctx)
-			if err != nil {
-				responseChan <- &reflectionResponse{
-					TargetError: TargetError{
-						TargetName: t.Config.Name,
-						Err:        err,
-					},
-				}
-				return
-			}
-			err = info.Send(&reflectpb.ServerReflectionRequest{
-				MessageRequest: &reflectpb.ServerReflectionRequest_ListServices{},
-			})
-			if err != nil {
-				responseChan <- &reflectionResponse{
-					TargetError: TargetError{
-						TargetName: t.Config.Name,
-						Err:        err,
-					},
-				}
-				return
-			}
-			rsp, err := info.Recv()
-
-			responseChan <- &reflectionResponse{
-				TargetError: TargetError{
-					TargetName: t.Config.Name,
-					Err:        err,
-				},
-				rsp: rsp,
-			}
-		}(t)
+		go a.reflectionServicesRequest(cmd.Context(), t, responseChan)
 	}
 	a.wg.Wait()
 	close(responseChan)
@@ -104,33 +46,16 @@ func (a *App) RunEServices(cmd *cobra.Command, args []string) error {
 		result = append(result, rsp)
 		a.printMsg(rsp.TargetName, rsp.rsp)
 	}
-
-	switch a.Config.Format {
-	default:
-		fmt.Println(a.reflectionServicesTable(result))
-	case "json":
-		for _, r := range result {
-			tRsp := targetResponse{
-				Target:   r.TargetName,
-				Response: r.rsp,
-			}
-			b, err := json.MarshalIndent(tRsp, "", "  ")
-			if err != nil {
-				a.Logger.Errorf("failed to marshal Services response from %q: %v", r.TargetName, err)
-				continue
-			}
-			fmt.Println(string(b))
-		}
-	}
+	a.printCMDOutput(result, a.reflectionServicesTable)
 	return a.handleErrs(errs)
 }
 
-func (a *App) reflectionServicesTable(r []*reflectionResponse) string {
-	targetTabData := make([][]string, 0, len(r))
-	sort.Slice(r, func(i, j int) bool {
-		return r[i].TargetName < r[j].TargetName
+func (a *App) reflectionServicesTable(rs []*reflectionResponse) string {
+	targetTabData := make([][]string, 0, len(rs))
+	sort.Slice(rs, func(i, j int) bool {
+		return rs[i].TargetName < rs[j].TargetName
 	})
-	for _, rsp := range r {
+	for _, rsp := range rs {
 		switch r := rsp.rsp.MessageResponse.(type) {
 		case *reflectpb.ServerReflectionResponse_ListServicesResponse:
 			for _, srv := range r.ListServicesResponse.GetService() {
@@ -139,10 +64,11 @@ func (a *App) reflectionServicesTable(r []*reflectionResponse) string {
 					srv.GetName(),
 				})
 			}
-
+		default:
+			a.Logger.Printf("%s: unexpected message type: %T", rsp.TargetName, rsp.rsp)
 		}
 	}
-	//
+
 	b := new(bytes.Buffer)
 	table := tablewriter.NewWriter(b)
 	table.SetHeader([]string{"Target Name", "Service"})
@@ -150,4 +76,76 @@ func (a *App) reflectionServicesTable(r []*reflectionResponse) string {
 	table.AppendBulk(targetTabData)
 	table.Render()
 	return b.String()
+}
+
+func (a *App) reflectionServicesRequest(ctx context.Context, t *api.Target, rspCh chan<- *reflectionResponse) {
+	defer a.wg.Done()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	ctx = metadata.AppendToOutgoingContext(ctx, "username", *t.Config.Username, "password", *t.Config.Password)
+
+	err := t.CreateGrpcClient(ctx, a.createBaseDialOpts()...)
+	if err != nil {
+		rspCh <- &reflectionResponse{
+			TargetError: TargetError{
+				TargetName: t.Config.Name,
+				Err:        err,
+			},
+		}
+		return
+	}
+	defer t.Close()
+
+	rfc := reflectpb.NewServerReflectionClient(t.Conn())
+	info, err := rfc.ServerReflectionInfo(ctx)
+	if err != nil {
+		rspCh <- &reflectionResponse{
+			TargetError: TargetError{
+				TargetName: t.Config.Name,
+				Err:        err,
+			},
+		}
+		return
+	}
+	req := &reflectpb.ServerReflectionRequest{
+		MessageRequest: &reflectpb.ServerReflectionRequest_ListServices{},
+	}
+	err = info.Send(req)
+	if err != nil {
+		rspCh <- &reflectionResponse{
+			TargetError: TargetError{
+				TargetName: t.Config.Name,
+				Err:        err,
+			},
+		}
+		return
+	}
+	rsp, err := info.Recv()
+	rspCh <- &reflectionResponse{
+		TargetError: TargetError{
+			TargetName: t.Config.Name,
+			Err:        err,
+		},
+		rsp: rsp,
+	}
+}
+
+func (a *App) printCMDOutput(rs []*reflectionResponse, fn func([]*reflectionResponse) string) {
+	switch a.Config.Format {
+	default:
+		fmt.Println(fn(rs))
+	case "json":
+		for _, r := range rs {
+			tRsp := targetResponse{
+				Target:   r.TargetName,
+				Response: r.rsp,
+			}
+			b, err := json.MarshalIndent(tRsp, "", "  ")
+			if err != nil {
+				a.Logger.Errorf("failed to marshal Target response from %q: %v", r.TargetName, err)
+				continue
+			}
+			fmt.Println(string(b))
+		}
+	}
 }
